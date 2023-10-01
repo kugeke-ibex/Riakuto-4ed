f2c9eb72cd66c8eb2a23c189f84175611a2c5cb3
Kenjiro Kuge <kugetyan0211@gmail.com>
2023-10-01 17:55:47 +0900

    Chapter2-5

---
+++ chapter-2/bird-proto.js
@@ -0,0 +1,29 @@
+function Bird(name) {
+    const className = "鳥類";
+    this.name = name;
+
+    this.cry = function (sound) {
+        console.log(`${this.name}が「${sound}」と鳴きました`);
+    };
+
+    this.introduce = () => {
+        console.log(`私は${className}の${this.name}です。`);
+    };
+
+    return this;
+}
+
+Bird.explain = function () {
+    console.log('これは鳥のクラスです');
+}
+
+function FlyableBird(name) {
+    Bird.call(this, name);
+    this.fly = function () {
+        console.log(`${this.name}が飛びました`);
+    }
+
+    return this
+}
+
+FlyableBird.prototype.__proto__ = Bird.prototype;
