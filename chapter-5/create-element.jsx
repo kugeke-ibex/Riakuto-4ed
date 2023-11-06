a4d278d8e626fc578da2cccd9a5336aa9552363a
Kenjiro Kuge <kugetyan0211@gmail.com>
2023-11-06 13:33:47 +0000

    Chapter5-1

---
+++ chapter-5/create-element.jsx
@@ -0,0 +1,18 @@
+import { React } from "react";
+
+React.createElement(
+    "button",
+    { type: "submit", autoFocus: true },
+    "Click Here!"
+);
+
+// 戻り値
+// {
+//   type: 'button',
+//   props: {
+//   type: 'submit',
+//   autoFocus: true,
+//   children: 'Click Here',
+//   },
+//   key: null,
+// };
