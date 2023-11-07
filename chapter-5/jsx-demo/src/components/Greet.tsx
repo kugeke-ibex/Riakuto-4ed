dcf07dad146a3bea0e8116ff3c4da6c50ef3eb09
Kenjiro Kuge <kugetyan0211@gmail.com>
2023-11-07 13:48:36 +0000

    Chapter5-2

--- chapter-5/jsx-demo/src/components/Greet.tsx
+++ chapter-5/jsx-demo/src/components/Greet.tsx
@@ -4,8 +4,8 @@ const Greet: React.FC<Props> = ({ name, times = 1 }) => {
     // const { name, times = 1 } = props;
     return (
         <>
-            {[...Array(times)].map(() => (
-                <p>Hello, {name}!</p>
+            {[...Array(times).keys()].map((i) => (
+                <p key={i}>Hello, {name}!</p>
             ))}
         </>
     );
