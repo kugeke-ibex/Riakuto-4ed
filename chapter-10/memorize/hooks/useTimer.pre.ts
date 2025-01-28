87e60079823f35db45a31c0394f557cec30bc4cf
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-01-28 14:53:45 +0000

    Chapter10-3

--- chapter-10/memorize/hooks/useTimer.pre.ts
+++ chapter-10/memorize/hooks/useTimer.pre.ts
@@ -1,19 +1,27 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, useRef } from "react";
 import type { SyntheticEvent } from "react";
 import { getPrimes } from "../utils/prime";
 
 export const useTimer = (maxCount: number): [number, boolean, () => void] => {
     const [timeLeft, setTimeLeft] = useState(maxCount);
     const primes = useMemo(() => getPrimes(maxCount), [maxCount]); // getPrimesが重い処理とした場合を考慮して、maxCountが変わるときに再計算する
+
+    const intervalId = useRef<ReturnType<typeof setInterval>>();
     const tick = () => setTimeLeft((t) => t - 1);
     const reset = (event?: SyntheticEvent) => {
         event?.stopPropagation();
+
+        if (intervalId.current !== undefined)  {
+            clearInterval(intervalId.current);
+        }
         setTimeLeft(maxCount);
+        intervalId.current = setInterval(tick, 1000);
     };
 
     useEffect(() => {
-        const timerId = setInterval(tick, 1000);
-        return () => clearInterval(timerId);
+        reset();
+        
+        return () => clearInterval(intervalId.current);
     }, []);
 
     useEffect(() => {
