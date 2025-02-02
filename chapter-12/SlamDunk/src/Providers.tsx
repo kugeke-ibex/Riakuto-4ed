fda460a0434b27841dc5106ace06d6611550b04e
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-02-02 15:52:34 +0000

    Chapter12-3

--- chapter-12/SlamDunk/src/Providers.tsx
+++ chapter-12/SlamDunk/src/Providers.tsx
@@ -1,11 +1,16 @@
 import type { FC, PropsWithChildren } from "react";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ThemeConfig } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/theme-utils";
 import { HelmetProvider } from "react-helmet-async";
 
-const theme = extendTheme({});
+const config: ThemeConfig = {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
 
 const Providers: FC<PropsWithChildren> = ({ children }) => (
     <HelmetProvider>
