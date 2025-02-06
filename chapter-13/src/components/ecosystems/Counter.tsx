c9a99c0f2b876f0ef97f10f2f2213b44d6cb6c3c
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-02-06 14:19:04 +0000

    Chapter13-2

---
+++ chapter-13/src/components/ecosystems/Counter.tsx
@@ -0,0 +1,22 @@
+import type { FC } from "react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { add, decrement, increment } from "../../stores/actions";
+import type { CounterState} from "../../stores/reducers";
+import CounterBoard from "../molecules/CounterBoard";
+
+const Counter: FC = () => {
+    const count = useSelector<CounterState, number>((state) => state.count);
+    const dispatch = useDispatch();
+
+    return (
+        <CounterBoard
+            count={count}
+            add={(amount: number) => dispatch(add(amount))}
+            decrement={() => dispatch(decrement())}
+            increment={() => dispatch(increment())}
+        />
+    );
+};
+
+export default Counter;
