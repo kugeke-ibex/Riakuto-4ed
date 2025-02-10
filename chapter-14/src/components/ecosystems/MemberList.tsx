f7d86472c21f0305190e039f635ff7626f6c1677
Kenjiro Kuge <kugetyan0211@gmail.com>
2025-02-10 16:20:45 +0000

    Chapter14-2

---
+++ chapter-14/src/components/ecosystems/MemberList.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import type { FC } from "react";
+import { Flex, Spinner } from "@chakra-ui/react";
+import { getMembers } from "domain/github";
+import type { User } from "domain/github";
+import UseList from "components/organism/UserList";
+
+type Props = {
+  orgCode: string;
+}
+
+const MemberList: FC<Props> = ({ orgCode }) => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const load = async () => {
+      setIsLoading(true);
+
+      try {
+        const usersData = await getMembers(orgCode);;
+        setUsers(usersData);
+      } catch (err) {
+        throw new Error(`organization '${orgCode}' not exists`);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    // 返ってくるPromiseをあえて無視することで、エラーを明示的にするためにvoid式を利用
+    void load();
+  }, [orgCode]);
+
+  return isLoading ? (
+    <Flex my={14} h="20rem" justify="center" align="center">
+      <Spinner size="xl" color="teal"/>
+    </Flex>
+  ) : (
+    <UseList users={users}/>
+  );
+};
+
+export default MemberList;
